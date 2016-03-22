946a37e25130ee38812449e0681bd80e5eaff73b
Edgar Zamora-Gómez <cesk002@gmail.com>
2016-03-22 11:06:13 +0100

    Change key of sorted nodes dict.

--- swift/proxy/server.py
+++ swift/proxy/server.py
@@ -435,7 +435,7 @@ class Application(object):
                 sorted_nodes_method = r.get("sorted_nodes_method")
                 sorted_nodes_criterion = r.get("sorted_nodes_criterion")
                 key_nodes = [n["ip"]+":"str(n["port"]+"/"+n["device"]) for n in nodes ]
-                values_for_sorting = r.hmget(sorted_nodes_method, key_nodes)
+                values_for_sorting = r.hmget("sorted_nodes:"+str(sorted_nodes_method), key_nodes)
 
                 for (n, sorting_value) in zip(nodes, values_for_sorting):
                     n["iostack_value"] = sorting_value
